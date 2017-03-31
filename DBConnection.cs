3180e70cc94f8699402b3886e316070025ffe036
Damian Darczuk <darczuk.damian@gmail.com>
2017-04-01 00:11:04 +0200

    Added reference to MySql.Data

---
+++ DBConnection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApplication1
+{
+    class DBConnection
+    {
+        private DBConnection()
+        {
+        }
+
+        private string databaseName = string.Empty;
+        public string DatabaseName
+        {
+            get { return databaseName; }
+            set { databaseName = value; }
+        }
+
+        public string Password { get; set; }
+        private MySqlConnection connection = null;
+        public MySqlConnection Connection
+        {
+            get { return connection; }
+        }
+
+        private static DBConnection _instance = null;
+        public static DBConnection Instance()
+        {
+            if (_instance == null)
+                _instance = new DBConnection();
+            return _instance;
+        }
+
+        public bool IsConnect()
+        {
+            bool result = true;
+            if (Connection == null)
+            {
+                if (String.IsNullOrEmpty(databaseName))
+                    result = false;
+                string connstring = string.Format("Server=localhost; database={0}; UID=UserName; password=your password", databaseName);
+                connection = new MySqlConnection(connstring);
+                connection.Open();
+                result = true;
+            }
+
+            return result;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+}
